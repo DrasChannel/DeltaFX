810eb1b0f561e8c8d219e636c0c9be184178039c
DrasChannel <appsepsi@gmail.com>
2023-01-29 14:18:49 +0100

    small changes

--- js/script.js
+++ js/script.js
@@ -1,2 +1,36 @@
 const swup = new Swup()
 
+/*
+if(window.location.href.indexOf("Home") !== -1){
+    console.log(1)
+    addEventListener('DOMContentLoaded', (event) => {
+        document.getElementById("navbar").style.background = "rgba(33, 33, 33, 0)"
+    });
+    window.onscroll = function() {
+        let scroll = (window.pageYOffset / 200).toString();
+        if(scroll>0.5){
+            scroll=0.5
+        }
+        console.log(scroll)
+        document.getElementById("navbar").style.backgroundColor = "rgba(33, 33, 33, "+scroll+")"
+    }
+}   
+function refreshscrollfade(){
+    if(window.location.href.indexOf("Home") !== -1){
+        console.log(1)
+        addEventListener('DOMContentLoaded', (event) => {
+            document.getElementById("navbar").style.background = "rgba(33, 33, 33, 0)"
+        });
+        window.onscroll = function() {
+            let scroll = (window.pageYOffset / 200).toString();
+            if(scroll>0.5){
+                scroll=0.5
+            }
+            console.log(scroll)
+            document.getElementById("navbar").style.backgroundColor = "rgba(33, 33, 33, "+scroll+")"
+        }
+    }   
+    else {
+        document.getElementById("navbar").style.background = "rgba(33, 33, 33, 0.5)"
+    }
+}*/
