8bdd2101d1427343d7e83c7e586c97110671e6f7
DrasChannel <appsepsi@gmail.com>
2023-01-29 20:09:06 +0100

    Fixed scrolling to the top and added navbar fade on homescreen

--- js/script.js
+++ js/script.js
@@ -1,36 +1,40 @@
 const swup = new Swup()
 
-/*
-if(window.location.href.indexOf("Home") !== -1){
-    console.log(1)
-    addEventListener('DOMContentLoaded', (event) => {
-        document.getElementById("navbar").style.background = "rgba(33, 33, 33, 0)"
-    });
-    window.onscroll = function() {
-        let scroll = (window.pageYOffset / 200).toString();
-        if(scroll>0.5){
-            scroll=0.5
-        }
-        console.log(scroll)
-        document.getElementById("navbar").style.backgroundColor = "rgba(33, 33, 33, "+scroll+")"
+// run once when page loads
+if (document.readyState === 'complete') {
+    scrollfade();
+} else {
+    document.addEventListener('DOMContentLoaded', () => scrollfade());
+}
+  
+// run after every additional navigation by swup
+swup.on('contentReplaced', scrollfade);
+
+// Define the function that does scrolling and fading
+let scrlfdFunc = function scrlfd() {
+    let scroll = (window.pageYOffset / 200).toString();
+    let scroll2 = (window.pageYOffset / 5).toString();
+    if(scroll>0.5){
+        scroll=0.5
     }
-}   
-function refreshscrollfade(){
+    if(scroll2>40){
+        scroll2=40
+    }
+    document.getElementById("navbar").style.backgroundColor = "rgba(33, 33, 33, "+scroll+")"
+    document.getElementById("navbar").style.backdropFilter = "blur("+scroll2+"px)"
+}
+
+// function that decides if the function above is going to be executed
+function scrollfade(){
+    window.scrollTo({top: 0,behavior: 'instant'});
     if(window.location.href.indexOf("Home") !== -1){
         console.log(1)
-        addEventListener('DOMContentLoaded', (event) => {
-            document.getElementById("navbar").style.background = "rgba(33, 33, 33, 0)"
-        });
-        window.onscroll = function() {
-            let scroll = (window.pageYOffset / 200).toString();
-            if(scroll>0.5){
-                scroll=0.5
-            }
-            console.log(scroll)
-            document.getElementById("navbar").style.backgroundColor = "rgba(33, 33, 33, "+scroll+")"
-        }
-    }   
-    else {
+        document.getElementById("navbar").style.background = "rgba(33, 33, 33, 0)"
+        document.getElementById("navbar").style.backdropFilter = "blur(0px)"
+        window.addEventListener("scroll", scrlfdFunc)
+    } else {
         document.getElementById("navbar").style.background = "rgba(33, 33, 33, 0.5)"
+        document.getElementById("navbar").style.backdropFilter = "blur(40px)"
+        window.removeEventListener("scroll", scrlfdFunc);
     }
-}*/
+}
