30a748fb1d594fd07c83fbe087127c488bbe9855
DrasChannel <appsepsi@gmail.com>
2023-02-06 14:29:51 +0100

    Navbar turned into sidebar

--- js/script.js
+++ js/script.js
@@ -2,83 +2,18 @@ const swup = new Swup()
 
 // run once when page loads
 if (document.readyState === 'complete') {
-    scrollfade();
-    addsearchbar();
+    scrolltotop();
 } else {
-    document.addEventListener('DOMContentLoaded', () => scrollfade());
-    document.addEventListener('DOMContentLoaded', () => addsearchbar());
+    document.addEventListener('DOMContentLoaded', () => scrolltotop());
 }
   
 // run after every additional navigation by swup
-swup.on('contentReplaced', scrollfade);
-swup.on('clickLink', addsearchbar);
+swup.on('contentReplaced', scrolltotop);
 
 // Define the function that does scrolling and fading
-let scrlfdFunc = function scrlfd() {
-    let scroll = (window.pageYOffset / 400).toString();
-    let scroll2 = (window.pageYOffset / 5).toString();
-    if(scroll>0.5){
-        scroll=0.5
-    }
-    if(scroll2>40){
-        scroll2=40
-    }
-    document.getElementById("navbar").style.backgroundColor = "rgba(33, 33, 33, "+scroll+")"
-    document.getElementById("navbar").style.backdropFilter = "blur("+scroll2+"px)"
-}
+
 
 // function that decides if the function above is going to be executed
-function scrollfade(){
+function scrolltotop(){
     window.scrollTo({top: 0,behavior: 'instant'});
-    if(window.location.href.indexOf("home") !== -1){
-        document.getElementById("navbar").style.background = "rgba(33, 33, 33, 0)"
-        document.getElementById("navbar").style.backdropFilter = "blur(0px)"
-        window.addEventListener("scroll", scrlfdFunc)
-    } else {
-        document.getElementById("navbar").style.background = "rgba(33, 33, 33, 0.5)"
-        document.getElementById("navbar").style.backdropFilter = "blur(40px)"
-        window.removeEventListener("scroll", scrlfdFunc);
-    }
-}
-
-// function to add searchbar
-function addsearchbar() {
-    setTimeout(function() {
-        if(window.location.href.indexOf("materials") !== -1){
-            document.getElementById("searchbar-container").style.width = "948px"
-            document.getElementById("searchbar-container").innerHTML = `
-            <div class="searchbar" id="searchbar-materials">
-                <input type="text" class="search-input" placeholder="Search materials" onfocus="focussearchbar()">
-            </div>
-            <div class="search-options">
-                <span class="search-options-title">Sort by</span>
-                <div class="search-option-releasedate">Latest</div>
-            </div>
-            `
-            document.getElementById("searchbar-container").style.opacity = "1"
-        } else if(window.location.href.indexOf("models") !== -1) {
-            document.getElementById("searchbar-container").style.width = "948px"
-            document.getElementById("searchbar-container").innerHTML = `
-            <div class="searchbar" id="searchbar-models">
-                <input type="text" class="search-input" placeholder="Search models" onfocus="focussearchbar()">
-            </div>
-            <div class="search-options">
-                <span class="search-options-title">Sort by</span>
-                <div class="search-option-releasedate">Latest</div>
-            </div>
-            `
-            document.getElementById("searchbar-container").style.opacity = "1"
-        } else {
-            document.getElementById("searchbar-container").innerHTML = ``
-            document.getElementById("searchbar-container").style.opacity = "0"
-            document.getElementById("searchbar-container").style.width = "0px"
-        }
-    }, 80);
-}
-
-function focussearchbar() {
-    document.querySelector(".searchbar").style.border = "solid 2px #1aa8ff"
-    document.querySelector(".search-input").addEventListener('blur', (event) => {
-        document.querySelector(".searchbar").style.border = "solid 2px #2b2b2b"
-    });
 }
