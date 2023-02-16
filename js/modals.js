dca9e5bd644fb6fb95343ccdf8beb5b1d9262153
DrasChannel <appsepsi@gmail.com>
2023-02-16 22:17:40 +0100

    privacy modal edit work in progress

--- js/modals.js
+++ js/modals.js
@@ -4,13 +4,24 @@ function showprivacypolicymodal(){
     <div class="modalbgclose" onclick="closemodal()"></div>
     <div class="modal" id="modal">
         <div class="modal-header">
-            <div class="modal-title">Privacy</div>
+            <div class="modal-title">Privacy policy</div>
             <div class="closebutton" onclick="closemodal()"></div>
         </div>
         <div class="modalbody">
-            This site uses no cookies! That's right, currently the site does not collect any
-            data or save cookie files, however this might change in the future if new features that
-            require it are implemented.
+
+            <h4>Last updated: February 16, 2023</h2>
+
+            <h3>Third Party cookies</h3>
+            The site doen't use any third party cookies
+
+            <h3>Performance cookies</h3>
+            The site uses cookies to find out how many files are you downloading at a time to improve
+            the performance of the server
+
+            <h3>Other cookies</h3>
+            work in progress
+            
+            
         </div>
     </div>`
     document.getElementById("modalbg").setAttribute("class", "active");
