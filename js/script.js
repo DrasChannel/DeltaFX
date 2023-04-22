96952ee1cdc8cb8d995836ec3323a41fc2533938
DrasChannel <appsepsi@gmail.com>
2023-04-22 10:57:43 +0200

    some updates

--- js/script.js
+++ js/script.js
@@ -6,11 +6,13 @@ if (document.readyState === 'complete') {
 } else {
     document.addEventListener('DOMContentLoaded', () => scrolltotop());
     document.addEventListener('DOMContentLoaded', () => generatemattreeview());
+    document.addEventListener('DOMContentLoaded', () => generatematthumbs());
 }
   
 // run after every additional navigation by swup
 swup.on('contentReplaced', scrolltotop);
 swup.on('contentReplaced', generatemattreeview);
+swup.on('contentReplaced', generatematthumbs);
 
 // Define the function that does scrolling and fading
 
@@ -25,20 +27,21 @@ function generatemattreeview(){
         document.getElementById("treeview").innerHTML = ""
         for(let i = 0; i < Object.keys(assetinfo).length; i++){
             let currentasset = Object.keys(assetinfo)[i]
-            let categoryassetcount = 1
+            
             if(document.getElementById(assetinfo[currentasset].category+"tcontainer") != null){
                 document.getElementById(assetinfo[currentasset].category+"subcategory").innerHTML += `
                 <div class="categorylinewrapper">
                     <span class="treeviewsubcategory" id="`+assetinfo[currentasset].category+assetinfo[currentasset].subcategory+`" name="`+assetinfo[currentasset].subcategory+`" onclick="selectsubcategory(this.id, '`+assetinfo[currentasset].category+`');categorysearch(this.id, '`+assetinfo[currentasset].category+`', this.attributes['name'].value)">`+assetinfo[currentasset].subcategory.charAt(0).toUpperCase() + assetinfo[currentasset].subcategory.slice(1)+`</span>
-                    <span class="categoryassetcount">1</span>
+                    <span class="categoryassetcount" id="categorysubassetcount`+i+`">1</span>
                 </div>
                 `
             } else {
+                
                 document.getElementById("treeview").innerHTML += `
                 <div class="treeviewcategorycontainer" id="`+assetinfo[currentasset].category+`tcontainer">
                 <div class="treeviewarrow" id="`+assetinfo[currentasset].category+`arrow" onclick="togglesubcategory('`+assetinfo[currentasset].category+`');removecategorysearch()"></div>
                 <span class="treeviewcategory" id="`+assetinfo[currentasset].category+`" name="`+assetinfo[currentasset].category+`" onclick="selectcategory(this.id);categorysearch(this.id, this.attributes['name'].value, this.attributes['name'].value)">`+assetinfo[currentasset].category.charAt(0).toUpperCase() + assetinfo[currentasset].category.slice(1)+`</span>
-                <span class="categoryassetcount">`+categoryassetcount+`</span>
+                <span class="categoryassetcount" id="categoryassetcount`+i+`">1</span>
                 </div>
                 `
                 document.getElementById("treeview").innerHTML += `
@@ -48,11 +51,29 @@ function generatemattreeview(){
                 document.getElementById(assetinfo[currentasset].category+"subcategory").innerHTML += `
                 <div class="categorylinewrapper">
                     <span class="treeviewsubcategory" id="`+assetinfo[currentasset].category+assetinfo[currentasset].subcategory+`" name="`+assetinfo[currentasset].subcategory+`" onclick="selectsubcategory(this.id, '`+assetinfo[currentasset].category+`');categorysearch(this.id, '`+assetinfo[currentasset].category+`', this.attributes['name'].value)">`+assetinfo[currentasset].subcategory.charAt(0).toUpperCase() + assetinfo[currentasset].subcategory.slice(1)+`</span>
-                    <span class="categoryassetcount">1</span>
+                    <span class="categoryassetcount" id="categorysubassetcount`+i+`">1</span>
                 </div>
                 `
             }
-            
+        }
+
+        for(let i = 0; i < Object.keys(assetinfo).length; i++){
+            let currentasset = Object.keys(assetinfo)[i]
+
+            document.getElementById("categoryassetcount"+i).innerHTML = document.getElementById(assetinfo[currentasset].category+"subcategory").childElementCount;
+        }
+    }
+}
+
+function generatematthumbs(){
+    if(window.location.href.indexOf("materials") > -1){
+        document.getElementById("maincontainer").innerHTML = ""
+        for(let i = 0; i < Object.keys(assetinfo).length; i++){
+            let currentasset = Object.keys(assetinfo)[i]
+
+            document.getElementById("maincontainer").innerHTML += `
+            <div class="asset-list-thumb" id="`+currentasset+`" onclick="showmatassetcard(this.id);enableselects();"><img src="assets/materials/`+currentasset+`/`+currentasset+`_thumbnail_640p.webp" class="img" alt="`+currentasset+`"><div class="thumbtitlecontainer"><span class="thumbtitle">`+assetinfo[currentasset].assetname+`</span></div></div>
+            `
         }
     }
 }
